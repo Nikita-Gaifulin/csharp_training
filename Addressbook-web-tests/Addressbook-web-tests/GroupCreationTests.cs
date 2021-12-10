de10eae39e0de875c190032cdb3f9036cf2f15aa
Nikita Gayfulin <Nikita.Gayfulin@emerson.com>
2021-12-10 15:34:45 +0300

    Add test for Contacts

--- Addressbook-web-tests/Addressbook-web-tests/GroupCreationTests.cs
+++ Addressbook-web-tests/Addressbook-web-tests/GroupCreationTests.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
 
 namespace addressbookWebTests
 {
